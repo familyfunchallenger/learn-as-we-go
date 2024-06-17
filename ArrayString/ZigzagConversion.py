046545d998de3a637f6a03cbb6d235fdb56a6ef7
Family Fun <familyfun@github.com>
2024-06-16 21:39:50 -0700

    solved the zigzag by using a 2-d array to actually fill it by going the specified way and then join the array to get the final return.

--- ArrayString/ZigzagConversion.py
+++ ArrayString/ZigzagConversion.py
@@ -44,4 +44,54 @@ class Solution:
         if numRows <= 1:
             return s
         ret = ''
+        c = (len(s) // (numRows + numRows - 2)) * (1 + numRows - 2) + 1
+        print('len of s: ', len(s))
+        print('num of columns: ', c)
+        print('num of rows: ', numRows)
+        arr = [''] * (c * numRows)
+        trend = -1 # -1: going down; 1: going zigzap up
+        i = 0
+        curr_row = 0
+        curr_col = 0
+        while i < len(s):
+            arr[curr_row * c + curr_col] = s[i]
+            print('s[i] - ', s[i])
+            
+            print('curr_row - ', curr_row)
+            print('curr_col - ', curr_col)
+            print(arr)
+            i += 1
+            if trend == -1:
+                # going down, update the row index
+                curr_row += 1
+                # check if next move should be going zigzag up
+                if curr_row == numRows:
+                    # need to back off one step up, and move one column forward
+                    trend = 1
+                    curr_row = numRows - 2
+                    curr_col += 1
+            else:
+                # going zigzag up now, update the row index first
+                curr_row -= 1
+                if curr_row < 0:
+                    curr_row = 1
+                    trend = -1
+                else:
+                    curr_col += 1
+        ret = ''.join(arr)
         return ret
+
+
+so = Solution()
+    
+s = "PAYPALISHIRING"
+numRows = 3
+print(so.convert(s, numRows))
+
+s = "PAYPALISHIRING"
+numRows = 4
+print(so.convert(s, numRows))
+
+s = "A"
+numRows = 1
+print(so.convert(s, numRows))
