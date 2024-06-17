c13433db63da219b0c422dbe8b2318690b297938
Family Fun <familyfun@github.com>
2024-06-17 13:07:25 -0700

    Done this in a bruteforce way. Need to see how others do it with some better approach.

--- ArrayString/TextJustification.py
+++ ArrayString/TextJustification.py
@@ -66,3 +66,84 @@ words[i] consists of only English letters and symbols.
 1 <= maxWidth <= 100
 words[i].length <= maxWidth
 """
+
+from typing import List
+
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        ret = []
+        i = 0
+        current_line_width = 0
+        current_line = []
+        lines = []
+        while i < len(words):
+            if current_line_width + len(current_line) + len(words[i]) - 1 <= maxWidth :
+                # add to current line 
+                current_line.append(words[i])
+                current_line_width += len(words[i])
+            else:
+                lines.append(current_line)
+                # re-initialize
+                current_line = [words[i]]
+                current_line_width = len(words[i])
+            i += 1
+            print('current_line - ', current_line)
+            print('current_line_width - ', current_line_width)
+        if current_line:
+            lines.append(current_line)
+        print(lines)
+        # Above can give the right lines, without padding
+        # Below is to do the padding
+        m = 0
+        while m < len(lines):
+            # For each line in lines, do the padding needed
+            baseSpaces = 1
+            needsForExtraSpaces = 0
+            current_line_width = sum([len(i) for i in lines[m]])
+            current_line = lines[m]
+            numWords = len(current_line)
+            if numWords > 1:
+                baseSpaces = (maxWidth - current_line_width) // (numWords - 1)
+                needsForExtraSpaces = (maxWidth - current_line_width) % (numWords - 1)
+            print()
+            print('current_line - ', current_line)
+            print('maxWidth - ', maxWidth)
+            print('numWords - ', numWords)
+            print('baseSpaces - ', baseSpaces)
+            print('needsForExtraSpaces - ', needsForExtraSpaces)
+            print('current_line_width - ', current_line_width)
+            lineStr = ''
+            if numWords == 1:
+                if len(current_line[0]) < maxWidth:
+                    lineStr = current_line[0] + ' ' * (maxWidth - len(current_line[0]))
+                else:
+                    lineStr = current_line[0]
+            else:
+                j = 0
+                while j < numWords - 1:
+                    if j < needsForExtraSpaces:
+                        lineStr += current_line[j] + (' ' * (baseSpaces + 1))
+                    else:
+                        lineStr += current_line[j] + (' ' * baseSpaces)
+                    j += 1
+                lineStr += ' ' * (maxWidth - len(lineStr) - len(current_line[numWords - 1])) + current_line[numWords - 1]
+            m += 1
+            ret.append(lineStr)
+                
+        return ret
+    
+    
+s = Solution()
+
+# TC1
+words = ["This", "is", "an", "example", "of", "text", "justification."]
+maxWidth = 16
+print(s.fullJustify(words, maxWidth))
+
+# TC2
+words = ["What","must","be","acknowledgment","shall","be"]
+maxWidth = 16
+
+# TC3
+words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"]
+maxWidth = 20
