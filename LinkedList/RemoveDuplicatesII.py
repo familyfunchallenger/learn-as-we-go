acbd72ddb3d32615d2a95ba0c44d574a9b9728d3
Your Name <you@example.com>
2024-06-27 09:03:39 -0700

    Finish the remove duplicates.

--- LinkedList/RemoveDuplicatesII.py
+++ LinkedList/RemoveDuplicatesII.py
@@ -17,9 +17,71 @@ The number of nodes in the list is in the range [0, 300].
 -100 <= Node.val <= 100
 The list is guaranteed to be sorted in ascending order.
 
-Solution should be straight forward:
-- having a pointer traversing the list from the head towards the end of the list in a loop 
-    * if current node's val is the same as the next node's val, remove next node
-    * if current node's val is different form the next node's val, move the pointer downwards one node
-
 """
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self) -> str:
+        return '[val: {0}; next {1}]'.format(self.val, self.next)
+    
+    def printList(self) -> None:
+        l = self.next
+        ret = [self.val]
+        while l is not None:
+            ret.append(l.val)
+            l = l.next
+        print(ret)
+
+
+def generateList(nums):
+    h = None
+    r = h
+    if nums is None or len(nums) == 0:
+        return h
+    else:
+        for n in nums:
+            p = ListNode(n)
+            if h is None:
+                h = p
+                r = p
+            else:
+                r.next = p # type: ignore
+                r = p
+    return h
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Solution should be straight forward:
+            - having a pointer traversing the list from the head towards the end of the list in a loop 
+                * if current node's val is the same as the next node's val, remove next node
+                * if current node's val is different form the next node's val, move the pointer downwards one node
+        """
+        if head is None or head.next is None:
+            return head
+        p = head
+        while p is not None:
+            p1 = p.next
+            if p1 is None:
+                break
+            if p.val == p1.val:
+                # we can remove p1
+                p.next = p1.next
+                p1.next = None
+                del p1
+            else:
+                p = p.next
+        return head
+    
+
+s = Solution()
+
+# head = [1,2,3,3,4,4,5]
+head = generateList([1, 2, 3, 3, 4, 4, 5])
+print(s.deleteDuplicates(head))
